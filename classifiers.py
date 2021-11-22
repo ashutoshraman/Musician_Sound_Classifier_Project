0ff3d23287a5f49239851d0cad9ccc26c16ce918
Ashu Raman <ashutosh.raman@duke.edu>
2021-11-22 08:51:48 -0500

    Begun classifier and pipeline script

---
+++ classifiers.py
@@ -0,0 +1,28 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC 
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import validation_curve
+from sklearn.model_selection import GridSearchCV
+
+# from plot_Boundary import plot_decision_boundary
+
+def k_nearest(argument):
+    pass
+
+def svm(argument):
+    pass
+
+def logistic(argument):
+    pass
+
+def gridsearch_and_pipeline(argument):
+    pass
